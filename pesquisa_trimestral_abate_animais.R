9ce7d2e1565ba55b76f9bfa333a7f0cfe1f38805
WillianDambros <126815136+WillianDambros@users.noreply.github.com>
2026-03-31 10:41:57 -0400

    Update pesquisa_trimestral_abate_animais.R

--- pesquisa_trimestral_abate_animais.R
+++ pesquisa_trimestral_abate_animais.R
@@ -11,24 +11,24 @@ frangos <- busca_ibge(1094, periodos = "202401-202504", localidades = "N3[all]")
 
 # Bovinos: adicionar "Bovinos - " em todas as categorias
 bovinos <- bovinos |> 
-  dplyr::rename(tipo_rebanho = `Tipo de rebanho bovino`) |>
+  dplyr::rename(tipo_conjunto = `Tipo de rebanho bovino`) |>
   dplyr::mutate(
-    tipo_rebanho = paste0("Bovinos - ", tipo_rebanho),
-    `Tipo de inspeção` = paste0("Bovinos - ", `Tipo de inspeção`)
+    tipo_conjunto = paste0("Bovinos - ", tipo_conjunto),
+    agregado = paste0("Bovinos Abatidos")
   )
 
 # Suínos: criar coluna tipo_rebanho com "Suínos - Total"
 suinos <- suinos |> 
   dplyr::mutate(
-    tipo_rebanho = "Suínos - Total",
-    `Tipo de inspeção` = paste0("Suínos - ", `Tipo de inspeção`)
+    tipo_conjunto = "Suínos - Total",
+    agregado = paste0("Suínos Abatidos")
   )
 
 # Frangos: criar coluna tipo_rebanho com "Frangos - Total"
 frangos <- frangos |> 
   dplyr::mutate(
-    tipo_rebanho = "Frangos - Total",
-    `Tipo de inspeção` = paste0("Frangos - ", `Tipo de inspeção`)
+    tipo_conjunto = "Frangos - Total",
+    agregado = paste0("Frangos Abatidos")
   )
 
 # ============================================
@@ -63,11 +63,6 @@ abate_animais <- abate_animais |>
 
 # Visualizar
 abate_animais |> dplyr::glimpse()
-
-# Verificar os valores
-abate_animais |>
-  dplyr::count(tipo_rebanho, `Tipo de inspeção`) |>
-  print(n = 30)
 
 # ============================================
 # SALVAR NO BANCO DE DADOS
